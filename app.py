ad84811f8db6f7957826ea3374e432207e92c089
Opeseen <Opeyemi5055@yahoo.com>
2023-02-13 00:00:17 +0100

    Deployed

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template;
+from flask import Flask, render_template, request;
 
 app = Flask(__name__)
 
-@app.route('/',methods = ['GET'])
+@app.route('/',methods = ['GET','POST'])
 def home():
-    return (render_template('index.html'))
+    if request.method == 'GET':
+        return (render_template('index.html'))
+    else:
+        username = request.form['Username']
+        password = request.form['Password']
+        if username == 'Opeyemi' and password == 'jembe':
+            return render_template('football.html',message = 'Login Successful')
+        else:
+            error_meassage = 'Wrong User Crendentials!'
+            return render_template('index.html', message = error_meassage)
 
+@app.route('/football',methods=['GET'])
+def football():
+    return render_template('football.html')
 
 if __name__ == '__main__':
     app.run(port=7000, debug=True)
---
+++ seed.py
@@ -0,0 +1,19 @@
+import sqlite3;
+
+connection = sqlite3.connect('flask_db',check_same_thread=False)
+cursor = connection.cursor()
+
+cursor.execute('''INSERT INTO users(username,password,favorite_color) VALUES('Opeyemi','1234','Red')''')
+cursor.execute('''INSERT INTO users(username,password,favorite_color) VALUES('Mike','1234','Blue')''')
+cursor.execute('''INSERT INTO users(username,password,favorite_color) VALUES('Tony','1234','Yellow')''')
+
+
+print("Data Inserted in the table: ")
+
+data = cursor.execute('''SELECT * FROM users''')
+for row in data:
+  print(row)
+
+connection.commit()
+cursor.close()
+connection.close()
