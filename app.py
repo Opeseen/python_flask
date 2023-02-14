14028e6fd4ca63ba016d06a9ddf45ee9a45b640a
Opeseen <Opeyemi5055@yahoo.com>
2023-02-15 00:30:41 +0100

    Deployed

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, flash;
+import model
+
 
 app = Flask(__name__)
 
@@ -9,19 +11,37 @@ def home():
     if request.method == 'GET':
         return (render_template('index.html'))
     else:
-        username = request.form['Username']
+        username = request.form['Username'].title()
         password = request.form['Password']
-        if username == 'Opeyemi' and password == 'jembe':
-            flash('Login Successful')
-            return render_template('football.html')
-            # return render_template('football.html',message = 'Login Successful')
+        db_passwd = model.check_passwd(username)
+
+        if password == db_passwd:
+            message = model.show_color(username)
+            return render_template('football.html',message = message)
         else:
-            error_meassage = 'Wrong User Crendentials!'
+            error_meassage = 'Wrong User credentials'
             return render_template('index.html', message = error_meassage)
+        
+
 
 @app.route('/football',methods=['GET'])
 def football():
     return render_template('football.html')
+
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    if request.method == 'GET':
+        message = 'Please Sign up...'
+        return render_template('signup.html',message = message)
+    else:
+        username = request.form['Username'].title()
+        password = request.form['Password']
+        favorite_color = request.form['favorite_color']
+        message = model.signup(username,password,favorite_color)
+
+        return render_template('signup.html',message = message)
+         
 
 if __name__ == '__main__':
     app.run(port=7000, debug=True)
---
+++ model.py
@@ -0,0 +1,43 @@
+import sqlite3;
+
+def show_color(username):
+    connection = sqlite3.connect('flask_db.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT favorite_color FROM users WHERE username = '{username}' ORDER BY ID DESC;")
+    color = cursor.fetchone()[0]
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    message = f"{username} Favorite color is {color}."
+    return message
+
+
+def check_passwd(username):
+    connection = sqlite3.connect('flask_db.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT password FROM users WHERE username = '{username}' ORDER BY ID DESC")
+    password = cursor.fetchone()[0]
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return password
+   
+
+def signup(username,password, favorite_color):
+    connection = sqlite3.connect('flask_db.db',check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT username FROM users WHERE username = '{username}' ORDER BY ID DESC;")
+    result = cursor.fetchone()
+
+    if result is None:
+        cursor.execute(f"INSERT INTO users(username,password,favorite_color) VALUES('{username}','{password}','{favorite_color}')")
+        connection.commit()
+        cursor.close()
+        connection.close()
+        
+        return (f"User {username} have been successfully SignUP...")
+    else:
+        return (f"User {username} Already Exist!!!")
+ 
